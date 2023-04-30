65d112e5682d843f117d2c022fddb7ac84eecddd
Amelie <andrewrb@mun.ca>
2023-04-29 21:48:42 -0230

    sync test

--- test.py
+++ test.py
@@ -30,13 +30,27 @@ with open('compsci.csv', newline='', encoding="utf8") as csvfile:
 
             messageDictionary.update({author:messageList})
 
-def chat(id) -> None: 
-    # search db for user
-    train(id)
+def talk(model, char_to_int, int_to_char) -> str:
+    evaluated = [char_to_int[s] for s in "be epic"]
+    evaluated = tf.expand_dims(evaluated, 0)
+
+    to_join = []
+    temp = 1.0
+    model.reset_states()
+    for i in range(100):
+        predict = model(evaluated)
+        predictions = tf.squeeze(predict, 0)
+        predict = predict/temp
+        predict_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+        evaluated = tf.expand_dims([predict_id], 0)
+        to_join.append(int_to_char[predict_id])
+    return ''.join(to_join)
+
+
 
 def train(id) -> None:
     
-    text = "\n".join(messageList)
+    text = "\n".join(messageDictionary.get(id))
     chars = sorted(list(set(text)))
     char_to_int = {ch:i for i, ch in enumerate(chars)}
     int_to_char = {i:ch for i, ch in enumerate(chars)}
@@ -46,17 +60,26 @@ def train(id) -> None:
     train_y = []
 
     for x in range(len(text)):
-        train_x.append([char_to_int[q] for q in text[x:x+longest_sequence]])
-        train_y.append(char_to_int[text[x+max_len]])
-    train_x = tf.keras.preprocessing.sequence.pad_sequences(X, maxlen=max_len, padding='post')
-    train_y = tf.keras.utils.to_categorical(y)
+        try:
+            train_x.append([char_to_int[q] for q in text[x:x+longest_sequence]])
+            train_y.append(char_to_int[text[x+longest_sequence]])
+        except:
+            train_x.pop()
+            continue
+    train_x = tf.keras.preprocessing.sequence.pad_sequences(train_x, maxlen=longest_sequence, padding='post')
 
-    model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Embedding(input_dim=len(chars), output_dim=64))
-    model.add(tf.keras.layers.LSTM(units=128))
-    model.add(tf.keras.layers.Dense(units=len(chars), activation='softmax'))
+    train_y = tf.keras.utils.to_categorical(train_y)
 
-    model.fit(X, y, epochs=3, batch_size=64)
+    model = tf.keras.Sequential([tf.keras.layers.Embedding(input_dim=len(chars), output_dim=64),
+    tf.keras.layers.LSTM(13728),
+    tf.keras.layers.Dense(2),            
+])
+
+    model.compile(optimizer='Adam', loss='sparse_categorical_crossentropy')
+    model.summary()
+    model.fit(train_x, train_y, epochs=1)
+    
+    print(talk(model,char_to_int,int_to_char))
 
 
 train(147670976551845888)
