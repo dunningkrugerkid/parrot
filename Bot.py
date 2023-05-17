a3de6997266537fbe1ba161131cfc69379c4bffd
Amelie <andrewrb@mun.ca>
2023-05-17 10:58:48 -0230

    training for everyone

--- Bot.py
+++ Bot.py
@@ -8,7 +8,8 @@ from generate import *
 
 load_dotenv()
 intents = discord.Intents.default()
-intents.message_content = True 
+intents.message_content = True
+intents.members = True
 client = discord.Client(intents = intents)
 messageDictionary = {}
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -36,7 +37,8 @@ async def on_ready() -> None:
             message = message.replace(",", " ")
 
             messageList = messageDictionary.get(author) if messageDictionary.get(author) is not None else []
-            messageList.append(message)
+            if not (message.startswith("`") and message.endswith("`")):
+                messageList.append(message)
 
             messageDictionary.update({author:messageList})
     print("load successful!")
@@ -58,6 +60,17 @@ async def on_message(message) -> None:
             async with message.channel.typing():
                 await message.channel.send(train(id, text, user.name, seed))
         else:
-            await message.channel.send('user not present in database')
+            await message.channel.send(user.name + ' not present in database')
+
+    elif message.content == "parrot train everyone!":
+        for member in message.guild.members:
+            id = member.id
+            if messageDictionary.get(id) is not None:
+                text = "\n".join(messageDictionary.get(id))
+                seed = "" + " ".join([x for x in message.content.split() if (not x.startswith("<@") and not(x.startswith("parrot!")))])
+                async with message.channel.typing():
+                    train(id, text, user.name, seed)
+                    await message.channel.send("completed training for " + user.name)
+
 
 client.run(TOKEN)
